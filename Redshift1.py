1a0d9ab248149f25c1829954a901f68902762fe4
aryaa-9 <125448847+aryaa-9@users.noreply.github.com>
2024-04-15 18:07:15 +0900

    Update Redshift1.py

--- Redshift1.py
+++ Redshift1.py
@@ -1,92 +1,95 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-get_ipython().run_line_magic('matplotlib', 'widget')
-from astropy.io import fits
 import numpy as np
+import scipy.signal
+from astropy.io import fits
 import matplotlib.pyplot as plt
-from astropy import units as u
-from astropy.visualization import quantity_support
-from specutils import Spectrum1D, SpectralRegion
-from specutils.fitting import fit_generic_continuum
-quantity_support()
+from scipy.optimize import curve_fit
 
 
-# In[2]:
+# The equation for a Gaussian:
+# 
+# G($x$) = $\frac{A}{\sigma\sqrt{2\pi}}e^{\frac{-(x-\mu)^2}{2\sigma^2}}$
+# 
+# A = amplitude
+# 
+# $\sigma$ = describes the width of the line
+# 
+# $x$ = is the array of values our gaussian spans
+# 
+# $\mu$ = the actual center of our gaussian
+
+
+def gauss(x, A, mean, sig):
+    #This function returns a Gaussian Profile
+    gaussian=((A/(sig*np.sqrt(2*np.pi)))*(np.exp(-((x-mean)**2)/(2*(sig**2)))))
+    return gaussian
+
+def nonlinearfunc(xvals, amp1, amp2, m1, m2, w1, cont):
+    #combining two gaussians
+    return gauss(xvals, amp1, m1, w1)+gauss(xvals, amp2, m2, w1)+cont
+
+def fitdata(wave, spec, amp1, amp2, m1, m2, w1, cont):
+    #fits two gaussian profiles to a sectrum to determine the amplitudes, line centers, widths, and the continuum level of the input spectrum
+    
+    print("================================================================================================================")
+    print("Performing the least squares fit: ")
+    
+    popt,pcov=curve_fit(nonlinearfunc, wave, spec, p0=[amp1, amp2, m1, m2, w1, cont])
+    errs=np.sqrt(np.diag(pcov))
+    
+    print("================================================================================================================")
+    print("Printing the best fitting parameters and 1 std errors: ")
+    print("________________________________________________________________________________________________________________")
+    print('line 1 Amp: ' + str(popt[0])+' Amp_err: ' + str(errs[0])+'\n')
+    print('line 2 Amp: ' + str(popt[1])+' Amp_err: ' + str(errs[1])+'\n')
+    print('line 1 Wavelength: ' + str(popt[2])+' Mean_4959_err: ' + str(errs[2])+'\n')
+    print('line 2 wavelength: ' + str(popt[3])+' Mean_5007_err: ' + str(errs[3])+'\n')
+    print('line width: ' + str(popt[4])+' Sigma_err: ' + str(errs[4])+'\n')
+    print('Continuum level: ' + str(popt[5])+' Cont_err: ' + str(errs[5])+'\n')
+    
+    print("================================================================================================================")
+    print("Printing the observed and modeled data: ")
+    ymodel=nonlinearfunc(wave, *popt)
+    
+    plt.scatter(wave, spec, label='data', color='tab:pink')
+    plt.plot(wave, ymodel, label='model', color='k')
+    plt.vlines(popt[2], ymin=0.0, ymax=spec.max(), linestyles='--', alpha=0.75, linewidth=1.0)
+    plt.vlines(popt[3], ymin=0.0, ymax=spec.max(), linestyles='--', alpha=0.75, linewidth=1.0)
+    
+    plt.legend()
+    plt.show()
+    return popt, pcov
+
+def getredshift(lamo, lame):
+    #reutrn the redshift of a source given the observed and rest frame wavelengths
+    return (lamo-lame)/lame
 
 
 hdul=fits.open('spec-0532-51993-0497.fits')
-data=hdul[1].data
+spec=hdul[1].data['flux']
+waves=10**hdul[1].data['loglam']
 
 
-# In[3]:
+sub_i=np.where((waves>6450)&(waves<6600))
+sub_spec=spec[sub_i]
+sub_wave=waves[sub_i]
 
-
-flux=hdul[1].data['flux']*10**-17*u.Unit('erg cm-2 s-2 AA-1')
-wave=10**hdul[1].data['loglam']*u.AA
-spec=Spectrum1D(spectral_axis=wave,flux=flux)
-spec.spectral_axis,spec.flux
-
-plt.figure()
-plt.plot(spec.spectral_axis,spec.flux)
-plt.title('Spectrum')
+fig=plt.figure()
+fig.set_size_inches(25,10)
+plt.plot(sub_wave, sub_spec)
 plt.show()
 
 
-# In[8]:
+specpeaks=scipy.signal.find_peaks(sub_spec, height=35)[0]
+print(sub_wave[specpeaks])
 
 
-sfit = fit_generic_continuum(spec)
-ycontfit = sfit(spec.spectral_axis)
+popt_, pcov_=fitdata(sub_wave, sub_spec, 40, 130, *sub_wave[specpeaks], 5.0, 4.0)
 
+l03_4959_e=4958.911
+l03_5007_e=5006.843
 
-# In[9]:
+src_redshift1=getredshift(popt_[2], l03_4959_e)
+print(src_redshift1)
 
-
-plt.figure()
-plt.plot(spec.spectral_axis, spec.flux)
-plt.plot(spec.spectral_axis, ycontfit)
-plt.show()
-
-
-# In[10]:
-
-
-plt.figure()
-plt.plot(spec.spectral_axis, ycontfit)  # corrected to use spectral_axis
-plt.show()
-
-
-# In[11]:
-
-
-plt.figure()
-plt.plot(spec.spectral_axis, flux - ycontfit)  # corrected to use spectral_axis
-plt.show()
-
-
-# In[12]:
-
-
-from scipy.interpolate import interp1d
-
-# Interpolate continuum fit
-continuum_interpolated = interp1d(spec.spectral_axis, ycontfit)
-
-# Define the wavelength point where you want to find the continuum value
-wavelength_point = 5500  # Example wavelength point in Angstroms
-
-# Find the continuum value at the specified wavelength point
-continuum_value_at_point = continuum_interpolated(wavelength_point * u.AA)
-
-print(f"At {wavelength_point} Angstroms, continuum value is: {continuum_value_at_point}")
-
-
-# In[ ]:
-
-
-
-
+src_redshift2=getredshift(popt_[3], l03_5007_e)
+print(src_redshift2)
